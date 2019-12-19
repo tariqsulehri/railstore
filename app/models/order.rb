dd07ec623128f7601794d4cdf707e2a9750081ee
Tariq Mahmood <tariq.sulehri@khaleef.com>
2019-12-19 12:04:50 +0500

    page180 order form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-    has_many :line_items, depentant: :destory
+    has_many :line_items, dependent: :destroy
 
     enum pay_type: {
         "Check"          => 0,
@@ -8,7 +8,7 @@ class Order < ApplicationRecord
     }
 
     validates :name, :address, :email, presence: true
-    validates :pay_type, inclusion: pay_types.key
+    validates :pay_type, inclusion: pay_types.keys
 
     def add_line_items_from_cart(cart)
         cart.line_items.each do |item|
